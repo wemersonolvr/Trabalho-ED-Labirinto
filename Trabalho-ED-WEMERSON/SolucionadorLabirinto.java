6d414e0f8c9fe578b2d905c397c0e9cd23d843fa
Wemerson <104464550+wemersonolvr@users.noreply.github.com>
2024-04-18 22:48:44 -0300

    Atualização do código
    implementação de fazer com que o código leia matrizes diretamente de arquivos .txt e melhorias nas funções deixando-as mais limpas dividindo algumas delas.

--- Trabalho-ED-WEMERSON/SolucionadorLabirinto.java
+++ Trabalho-ED-WEMERSON/SolucionadorLabirinto.java
@@ -1,11 +1,14 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
 
 public class SolucionadorLabirinto {
     private char[][] labirinto;
     private boolean[][] visitado;
     private int linhas;
     private int colunas;
-    private int[][] direcoes = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // Baixo, Cima, Direita, Esquerda
+    private int[][] direcoes = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } }; // Baixo, Cima, Direita, Esquerda
     private static final int DELAY_MS = 800; // Atraso em milissegundos (200ms)
 
     public SolucionadorLabirinto(char[][] labirinto) {
@@ -14,60 +17,74 @@ public class SolucionadorLabirinto {
         this.colunas = labirinto[0].length;
         this.visitado = new boolean[linhas][colunas];
     }
+      //função para encontrar o caminho (ou não)
+    public boolean encontrarCaminho(int linhaInicial, int colunaInicial) {
+        Stack<Posicao> pilha = new Stack<>();
+        pilha.push(new Posicao(linhaInicial, colunaInicial));
 
-  public boolean encontrarCaminho(int linhaInicial, int colunaInicial) {
-    Stack<Posicao> pilha = new Stack<>();
-    pilha.push(new Posicao(linhaInicial, colunaInicial));
+        while (!pilha.isEmpty()) {
+            if (explorarCaminho(pilha)) {
+                imprimirCaminho();
+                System.out.println("Caminho encontrado!");
+                return true;
+            }
+        }
 
-    while (!pilha.isEmpty()) {
+        System.out.println("Nenhum caminho encontrado!");
+        return false;
+    }
+      //função para explorar caminhos
+    private boolean explorarCaminho(Stack<Posicao> pilha) {
         Posicao posicaoAtual = pilha.peek();
         int linha = posicaoAtual.getLinha();
         int coluna = posicaoAtual.getColuna();
 
         if (movimentoValido(linha, coluna)) {
             if (labirinto[linha][coluna] == 'D') { // Destino encontrado
-                labirinto[linha][coluna] = 'F'; // Altera 'D' para 'F' (indicando fim)
-                imprimirCaminho();
-                System.out.println("Caminho encontrado!");
+                labirinto[linha][coluna] = 'F'; // Altera de 'D' para 'F' indicando "fim", mostrando que encontrou o destino
                 return true;
-
             }
 
             visitado[linha][coluna] = true;
 
-            boolean movimentoRealizado = false;
-            for (int[] direcao : direcoes) {
-                int novaLinha = linha + direcao[0];
-                int novaColuna = coluna + direcao[1];
-                if (movimentoPossivel(novaLinha, novaColuna)) {
-                    pilha.push(new Posicao(novaLinha, novaColuna));
-                    movimentoRealizado = true;
-                }
-            }
+            boolean movimentoRealizado = realizarMovimentos(pilha, linha, coluna);
 
             if (!movimentoRealizado) {
-                pilha.pop(); // Se não houver movimentos possíveis, remova a posição atual da pilha
+                pilha.pop(); // Se não houver movimentos possíveis, remove a posição atual da pilha
             }
 
-            imprimirCaminho();
-
-            try {
-                Thread.sleep(DELAY_MS); // Atraso após cada movimento
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            return false;
         } else {
-            pilha.pop(); // Se a posição atual não for válida, remova da pilha
+            pilha.pop(); // Se a posição atual não for válida, remove da pilha
+            return false;
         }
     }
+      //função que realiza e imprime o caminho com o delay
+    private boolean realizarMovimentos(Stack<Posicao> pilha, int linha, int coluna) {
+        boolean movimentoRealizado = false;
+        for (int[] direcao : direcoes) {
+            int novaLinha = linha + direcao[0];
+            int novaColuna = coluna + direcao[1];
+            if (movimentoPossivel(novaLinha, novaColuna)) {
+                pilha.push(new Posicao(novaLinha, novaColuna));
+                movimentoRealizado = true;
+            }
+        }
 
-    System.out.println("Nenhum caminho encontrado!");
-    return false;
-}
+        imprimirCaminho();
 
+        try {
+            Thread.sleep(DELAY_MS); // Delay de movimento para ser possível ver o "robô" andando
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return movimentoRealizado;
+    }
 
     private boolean movimentoValido(int linha, int coluna) {
-        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas && labirinto[linha][coluna] != '1' && !visitado[linha][coluna];
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas && labirinto[linha][coluna] != '1'
+                && !visitado[linha][coluna];
     }
 
     private boolean movimentoPossivel(int linha, int coluna) {
@@ -78,7 +95,7 @@ public class SolucionadorLabirinto {
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
                 if (visitado[i][j]) {
-                    System.out.print("X "); // Marcar posições visitadas como 'X'
+                    System.out.print("X "); // Marcar posições visitadas com 'X'
                 } else {
                     System.out.print(labirinto[i][j] + " ");
                 }
@@ -87,4 +104,42 @@ public class SolucionadorLabirinto {
         }
         System.out.println();
     }
- }
+
+    public static char[][] lerLabirintoDoArquivo(String nomeArquivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            int linhas = 0;
+            int colunas = 0;
+
+            // Contar linhas e colunas
+            while ((linha = br.readLine()) != null) {
+                linhas++;
+                if (colunas == 0) {
+                    colunas = linha.length();
+                }
+            }
+
+            // Reiniciar o leitor
+            try (BufferedReader br2 = new BufferedReader(new FileReader(nomeArquivo))) {
+                char[][] labirinto = new char[linhas][colunas];
+                int linhaAtual = 0;
+
+                // Preenche a matriz do labirinto
+                while ((linha = br2.readLine()) != null) {
+                    if (linha.trim().isEmpty()) {
+                        continue; // Ignora linhas em branco
+                    }
+                    for (int i = 0; i < colunas; i++) {
+                        labirinto[linhaAtual][i] = linha.charAt(i);
+                    }
+                    linhaAtual++;
+                }
+
+                return labirinto;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- Trabalho-ED-WEMERSON/main.java
+++ Trabalho-ED-WEMERSON/main.java
@@ -1,38 +1,32 @@
- import java.util.*;
- import java.io.*;
- 
- public class main{
-  public static void main(String[] args) {
-        char[][] labirinto = {
-                {'1', '1', '1', '1', '1', '1', '1', '1'},
-                {'1', 'S', '0', '0', '0', '0', '0', '1'},
-                {'1', '1', '1', '1', '1', '1', '0', '1'},
-                {'1', '1', '1', '0', '0', '1', '0', '1'},
-                {'1', '0', '0', '0', '1', '1', '0', '1'},
-                {'1', '0', '1', '0', '0', '0', '0', '1'},
-                {'1', 'D', '1', '1', '1', '1', '0', '1'},
-                {'1', '1', '1', '1', '1', '1', '1', '1'}
-        };
+import java.util.Stack;
 
-        int linhaInicial = -1;
-        int colunaInicial = -1;
+public class main {
+    public static void main(String[] args) {
+        char[][] labirinto = SolucionadorLabirinto.lerLabirintoDoArquivo("matriz.txt");
 
-        // Encontrar a posição inicial 'S' no labirinto
-        for (int i = 0; i < labirinto.length; i++) {
-            for (int j = 0; j < labirinto[0].length; j++) {
-                if (labirinto[i][j] == 'S') {
-                    linhaInicial = i;
-                    colunaInicial = j;
-                    break;
+        if (labirinto != null) {
+            int linhaInicial = -1;
+            int colunaInicial = -1;
+
+            // Encontrar a posição inicial 'S' no labirinto
+            for (int i = 0; i < labirinto.length; i++) {
+                for (int j = 0; j < labirinto[0].length; j++) {
+                    if (labirinto[i][j] == 'S') {
+                        linhaInicial = i;
+                        colunaInicial = j;
+                        break;
+                    }
                 }
             }
-        }
 
-        if (linhaInicial != -1 && colunaInicial != -1) {
-            SolucionadorLabirinto solucionador = new SolucionadorLabirinto(labirinto);
-            solucionador.encontrarCaminho(linhaInicial, colunaInicial); // Posição inicial
+            if (linhaInicial != -1 && colunaInicial != -1) {
+                SolucionadorLabirinto solucionador = new SolucionadorLabirinto(labirinto);
+                solucionador.encontrarCaminho(linhaInicial, colunaInicial); // Posição inicial
+            } else {
+                System.out.println("Posição inicial 'S' não encontrada!");
+            }
         } else {
-            System.out.println("Posição inicial 'S' não encontrada!");
+            System.out.println("Erro ao ler o labirinto do arquivo!");
         }
     }
 }
